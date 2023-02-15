6d93af992b9737c4b7da6a7926db5cccf9898de8
Cameron Coleman <c.d.coleman@hotmail.com>
2023-02-14 21:41:32 -0600

    update save button

--- scripts/index.js
+++ scripts/index.js
@@ -25,14 +25,30 @@ let initialCards = [
   },
 ];
 
+// select buttons
 let editButton = document.querySelector(".profile__button-edit");
 let closeButton = document.querySelector(".modal__container-close");
 let modalSelector = document.querySelector(".modal");
+let saveButton = document.querySelector(".modal__save-button");
+let username = document.querySelector(".profile__name");
+let modalName = document.querySelectorAll(".modal__input")[0];
+let description = document.querySelector(".profile__subtitle");
+let modalDescription = document.querySelectorAll(".modal__input")[1];
 
+// open and close pop-up && render modal text
 editButton.addEventListener("click", function () {
   modalSelector.classList.add("modal_opened");
+  modalName.value = username.textContent;
+  modalDescription.value = description.textContent;
 });
 
 closeButton.addEventListener("click", function () {
+  modalSelector.classList.remove("modal_opened");
+});
+
+// save button
+saveButton.addEventListener("click", function () {
+  username.textContent = modalName.value;
+  description.textContent = modalDescription.value;
   modalSelector.classList.remove("modal_opened");
 });
